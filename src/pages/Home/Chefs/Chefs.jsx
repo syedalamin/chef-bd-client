ab39c8961485546f25ebcbc8792c41895a729c76
SyedAlamin <syedalamin2580@gmail.com>
2023-05-02 03:36:18 +0600

    single chef

--- src/pages/Home/Chefs/Chefs.jsx
+++ src/pages/Home/Chefs/Chefs.jsx
@@ -1,25 +1,27 @@
-import React , {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import SingleChef from '../SingleChef/SingleChef';
 const Chefs = () => {
     const [chef, setChef] = useState([]);
     console.log(chef);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:5000/chef')
-        .then(res => res.json())
-        .then(data => setChef(data))
-        .catch(error => console.log(error))
-    },[])
+            .then(res => res.json())
+            .then(data => setChef(data))
+            .catch(error => console.log(error))
+    }, [])
 
     return (
         <div className='text-center'>
             <h4>All Shef {chef.length}</h4>
-            {
-               chef.map(che => 
-               <SingleChef 
-                key={che.id}
-                chef={che}
-               ></SingleChef>)
-            }
+            <div className='grid grid-cols-3'>
+                {
+                    chef.map(che =>
+                        <SingleChef
+                            key={che.id}
+                            chef={che}
+                        ></SingleChef>)
+                }
+            </div>
         </div>
     );
 };
--- src/pages/Home/SingleChef/SingleChef.jsx
+++ src/pages/Home/SingleChef/SingleChef.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const SingleChef = ({chef}) => {
-    console.log(chef)
+const SingleChef = ({ chef }) => {
+    const {image_url, id, name } = chef;
 
     return (
         <div>
-            <div className=''>
-
+            <div className="card w-96 mx-auto bg-base-100 gap-4 shadow-xl">
+                <figure><img style={{height: '300px'}} src={image_url} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
